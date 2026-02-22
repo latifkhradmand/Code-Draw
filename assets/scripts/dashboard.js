640a335c7c04a5d137957b6288ca8e24f173161b
Latif Khradmand <lh.latif.936@gmaail.com>
2026-02-22 23:08:21 +0330

    respnisive vere

--- assets/scripts/dashboard.js
+++ assets/scripts/dashboard.js
@@ -5,6 +5,5 @@ const contentChart = document.getElementById("content");
 profileButton.addEventListener("click", () => {
   profileMenu.classList.toggle("hidden");
   console.log("object")
-
   contentChart.classList.toggle("hidden");
 });
